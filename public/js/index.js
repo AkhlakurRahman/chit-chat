9acc20b5c3fb89d4c5fbca5db6aceadcd627a5f2
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-01-19 00:39:53 +0600

    Format timestamp using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -1,83 +1,66 @@
 const socket = io();
 
-socket.on('connect', () => {
-    console.log('Connected to server');
+socket.on('connect', function () {
+	console.log('Connected to server');
 });
 
-socket.on('disconnect', () => {
-    console.log('Disconnected from server');
+socket.on('disconnect', function () {
+	console.log('Disconnected from server');
 });
 
-socket.on('newEmail', (email) => {
-    console.log('New email', email);
+socket.on('newMessage', function (message) {
+	const formattedTime = moment(message.createdAt).format('h:mm a');
+	const li = $('<li class="list-group-item"></li>');
+	const small = $(`<br><i><small class="ml-2">at ${formattedTime}</small></i>`);
+	li.text(`${message.from}: ${message.text}`);
+	li.append(small);
+
+
+	$('#messages').append(li);
 });
 
-socket.on('newMessage', (message) => {
-    //console.log('Message', message);
+socket.on('newLocationMessage', function (message) {
+	const formattedTime = moment(message.createdAt).format('h:mm a');
+	const li = $('<li class="list-group-item"></li>');
+	const a = $('<a class="text-success" target="_blank">My current location</a>');
+	const small = $(`<br><i><small class="ml-2">at ${formattedTime}</small></i>`);
 
-    const li = $('<li class="list-group-item"></li>');
-
-    li.text(`${message.from}: ${message.text}`);
-    $('.list-group').append(li);
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+	li.append(a);
+	li.append(small);
+	$('#messages').append(li);
 });
 
-socket.on('newLocationMessage', (message) => {
-    const li = $('<li class="list-group-item"></li>');
-    const a = $('<a class="text-success" target="_blank"> My current location</a>');
+$('#message-form').on('submit', function (e) {
+	e.preventDefault();
 
-    li.text(`${message.from}`);
-    a.attr('href', message.url);
-    li.append(a);
+	const messageTextbox = $('[name=message]');
 
-    $('.list-group').append(li);
+	socket.emit('createMessage', {
+		from: 'User',
+		text: messageTextbox.val()
+	}, function () {
+		messageTextbox.val('')
+	});
 });
 
-$('#message').on('submit', function (e) {
-    e.preventDefault();
-
-    socket.emit('createMessage', {
-        from: 'User',
-        text: $('[name=message_input]').val()
-    }, () => {
-
-    })
-});
-
-const locationButton = $('#send_location');
-
+const locationButton = $('#send-location');
 locationButton.on('click', function () {
-    if (!navigator.geolocation) {
-        return alert('Your browser does not support geolocation');
-    }
+	if (!navigator.geolocation) {
+		return alert('Geolocation not supported by your browser.');
+	}
 
-    navigator.geolocation.getCurrentPosition(function (position) {
-        socket.emit('createLocationMessage', {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-        })
-    }, function () {
-        alert('Unable to fetch location!');
-    })
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+	navigator.geolocation.getCurrentPosition(function (position) {
+		locationButton.removeAttr('disabled').text('Send location');
+		socket.emit('createLocationMessage', {
+		  latitude: position.coords.latitude,
+		  longitude: position.coords.longitude
+		});
+	}, function () {
+		locationButton.removeAttr('disabled').text('Send location');
+		alert('Unable to fetch location.');
+	});
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- server/server.js
+++ server/server.js
@@ -10,35 +10,30 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
+	console.log('New user connected');
 
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to Chit-Chat'));
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user has joined'));
+	socket.on('createMessage', (message, callback) => {
+		console.log('createMessage', message);
+		io.emit('newMessage', generateMessage(message.from, message.text));
+		callback();
+	});
 
-    socket.on('createMessage', (message, callback) => {
-        //console.log('From user', message);
+	socket.on('createLocationMessage', (coords) => {
+		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+	});
 
-        io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from server!');
-    });
-
-    socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-    })
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+	socket.on('disconnect', () => {
+		console.log('User was disconnected');
+	});
 });
 
-
-
 server.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+	console.log(`Server is up on ${port}`);
 });
